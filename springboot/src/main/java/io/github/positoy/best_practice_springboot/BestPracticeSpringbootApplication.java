1b40c9dd450a728d71e8020993eb57391a158953
positoy <positoy@gmail.com>
2025-06-30 10:46:37 +0900

    feat: start springboot with data+jdbc+lombok

---
+++ springboot/src/main/java/io/github/positoy/best_practice_springboot/BestPracticeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.github.positoy.best_practice_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestPracticeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestPracticeSpringbootApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/io/github/positoy/best_practice_springboot/BestPracticeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.positoy.best_practice_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestPracticeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
